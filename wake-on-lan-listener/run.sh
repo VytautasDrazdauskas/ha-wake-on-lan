25d998b6c6371d75dc97a8c40c072be22db10bba
VytautasDrazdauskas <35038845+VytautasDrazdauskas@users.noreply.github.com>
2025-02-16 00:56:57 +0200

    Delete wake-on-lan-listener/run.sh

--- wake-on-lan-listener/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bash
-set -e
-
-echo "Starting Wake-on-LAN Listener add-on..."
-
-# Run the main Python script under the correct user
-exec s6-setuidgid root python3 /app/main.py
